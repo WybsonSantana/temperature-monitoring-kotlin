fded7de67ba5237d14a991430d5058ab3d7fe6c7
WybsonSantana <wybson@hotmail.com>
2025-04-12 01:01:08 -0300

    KSensors: #01 - 08.08 - Multiprojects with IntelliJ IDEA

---
+++ src/test/kotlin/br/dev/s2w/jsensors/temperature/monitoring/TemperatureMonitoringApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.dev.s2w.jsensors.temperature.monitoring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemperatureMonitoringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
